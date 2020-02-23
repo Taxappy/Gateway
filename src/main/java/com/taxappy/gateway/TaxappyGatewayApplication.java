e24c987a175e9b56c69d06c099cdc09fc7796810
eduardo velasquez <eduardo.velasquezv@udea.edu.co>
2020-02-23 16:51:00 -0500

    microservicio actualizado y funcionando

--- src/main/java/com/taxappy/gateway/TaxappyGatewayApplication.java
+++ src/main/java/com/taxappy/gateway/TaxappyGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.taxapy.zuulgateway;
+package com.taxappy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +8,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableEurekaClient // It acts as a eureka client
 @EnableZuulProxy // Enable Zuul
-public class TaxapyGatewayApplication {
+public class TaxappyGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaxapyGatewayApplication.class, args);
+		SpringApplication.run(TaxappyGatewayApplication.class, args);
 	}
+
 }
